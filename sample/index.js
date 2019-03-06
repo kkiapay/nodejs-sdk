3a2508fe85881aaa643447a5cedd1b2ba333808a
Latif Boussari <latifboussari@MacBook-Pro-de-Latif.local>
2019-03-06 11:11:19 +0100

    re-export of the library

--- sample/index.js
+++ sample/index.js
@@ -1,7 +1,7 @@
 const kkiapay=require("../index")
 
 
-kkiapay.transaction({ transactionId:"xxxxxx",apikey:"xxxxxxx"}).
+kkiapay.transaction({ transactionId:"RUYnpqsSQ1",apikey:process.env.apikey}).
 then(response => console.log(response)).
 catch(err => console.log(err))
 
