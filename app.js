928f6c15d74bd7760c849280b36094b20f65ba5d
Latif Boussari <latifboussari@MacBook-Pro-de-Latif.local>
2019-03-06 11:07:46 +0100

    re-export of the library

--- app.js
+++
@@ -1,5 +0,0 @@
-import VerifyStatus from "./src/VerifyStatus";
-
-VerifyStatus("xDNoswash1").
-then(response => console.log(response)).
-catch(err => console.log(err))
--- sample/index.js
+++ sample/index.js
@@ -1,10 +1,7 @@
-require('../lib/opts').baseURL = 'https://api-dev.kkiapay.me'
-const {transaction} = require("../lib")
+const kkiapay=require("../index")
 
 
-transaction({ transactionId:"xxxxxx",apikey:"xxxxxxx"}).
+kkiapay.transaction({ transactionId:"xxxxxx",apikey:"xxxxxxx"}).
 then(response => console.log(response)).
 catch(err => console.log(err))
-
-
 
