cb4eae51d7eb8a9a1ee2d2e727627e0a45811c6d
Latif Boussari <latifboussari@MacBook-Pro-de-Latif.local>
2019-03-06 10:34:07 +0100

    fix bun

--- sample/index.js
+++ sample/index.js
@@ -2,7 +2,7 @@ require('../lib/opts').baseURL = 'https://api-dev.kkiapay.me'
 const {transaction} = require("../lib")
 
 
-transaction({ transactionId:"xDNoswash1",apikey:"c27fbe803bb211e9b9cc57122c11a69f"}).
+transaction({ transactionId:"xxxxxx",apikey:"xxxxxxx"}).
 then(response => console.log(response)).
 catch(err => console.log(err))
 
