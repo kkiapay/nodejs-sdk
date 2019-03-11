4843d20abc1efe8dc71d06f979c5e4aba8daa622
Dossou Fiacre AYEDOUN <fiacre.ayedoun@gmail.com>
2019-03-11 20:01:42 +0100

    configuration

--- sample/index.js
+++ sample/index.js
@@ -1,7 +1,7 @@
 const kkiapay=require("../index")
 
 
-kkiapay.transaction({ transactionId:"RUYnpqsSQ1",privatekey:process.env.privatekey,publickey:process.env.publickey,secretkey:process.env.secretkey}).
+kkiapay.transaction({ transactionId: "RUYnpqsSQ1", privatekey: process.env.privatekey, publickey: process.env.publickey, secretkey: process.env.secretkey }).
 then(response => console.log(response)).
 catch(err => console.log(err))
 
