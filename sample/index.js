3c2f179f7947800b8ea89d3d37b5026d92ca3c5b
Latif Boussari <latifboussari@MacBook-Pro-de-Latif.local>
2019-04-05 09:08:24 +0100

    initialize api with object

--- sample/index.js
+++ sample/index.js
@@ -1,7 +1,13 @@
-const kkiapay=require("../index")
+const kkiapay=require("../lib")
 
 
-kkiapay.transaction({ transactionId:"RUYnpqsSQ1",privatekey:process.env.privatekey,publickey:process.env.publickey,secretkey:process.env.secretkey}).
-then(response => console.log(response)).
-catch(err => console.log(err))
+let k=kkiapay({ publickey:"42051160403a11e98f723752f967fed2",privatekey:"hjk",secretkey:"gvhjb"})
+
+k.verify("JkgWE6DO2C").
+then(e=>console.log(e)).
+catch(err=>console.log(err))
+
+// kkiapay.transaction({ transactionId:"JkgWE6DO2C",privatekey:"",publickey:"42051160403a11e98f723752f967fed2",secretkey:""}).
+// then(response => console.log(response)).
+// catch(err => console.log(err))
 
