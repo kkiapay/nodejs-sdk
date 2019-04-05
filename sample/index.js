6272cde7db9034aab4ef8c2916ea0a9e106199f3
Latif Boussari <latifboussari@MacBook-Pro-de-Latif.local>
2019-04-05 09:10:39 +0100

    add sample file

--- sample/index.js
+++ sample/index.js
@@ -7,7 +7,5 @@ k.verify("JkgWE6DO2C").
 then(e=>console.log(e)).
 catch(err=>console.log(err))
 
-// kkiapay.transaction({ transactionId:"JkgWE6DO2C",privatekey:"",publickey:"42051160403a11e98f723752f967fed2",secretkey:""}).
-// then(response => console.log(response)).
-// catch(err => console.log(err))
+
 
