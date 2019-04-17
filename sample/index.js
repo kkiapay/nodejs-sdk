997e3d6c89df643c6ad071a3a8537b9f6dd6be5c
lephenix1234 <38128001+lephenix1234@users.noreply.github.com>
2019-04-17 17:25:09 +0100

    Update index.js

--- sample/index.js
+++ sample/index.js
@@ -3,7 +3,7 @@ const kkiapay=require("../lib")
 
 let k=kkiapay({ publickey:"process.env.publickey",privatekey:"process.env.privatekey",secretkey:"process.env.secretkey"})
 
-k.verify("xxxxxx").
+k.verify("AN5uUl574Z").
 then(e=>console.log(e)).
 catch(err=>console.log(err))
 
