1d154243f4e11437f78311b195de122aeea5a389
Boussari Latif <latif.boussari@mehriso.com>
2019-06-27 12:26:15 +0100

    add sandbox environnement

--- sample/index.js
+++ sample/index.js
@@ -1,9 +1,9 @@
 const kkiapay=require("../lib")
 
 
-let k=kkiapay({ publickey:"process.env.publickey",privatekey:"process.env.privatekey",secretkey:"process.env.secretkey"})
+let k=kkiapay({publickey:"f774e960812811e99cf7e73f32bcec42",privatekey:"process.env.privatekey",secretkey:"process.env.secretkey",sandbox:true})
 
-k.verify("AN5uUl574Z").
+k.verify("O_S9_rdlX").
 then(e=>console.log(e)).
 catch(err=>console.log(err))
 
