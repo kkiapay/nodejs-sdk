d0695db0d9108c69edfe79b2497400db0bcafb11
erickadje <erickadje96@gmail.com>
2019-06-28 15:33:10 +0100

    add of sanbox mode and scheduler payout functionnality

--- sample/index.js
+++ sample/index.js
@@ -1,6 +1,6 @@
 const kkiapay=require("../lib")
 
-let k=kkiapay({publickey:"e4e6e19080ab11e99cf7e73f32bcec42",privatekey:"tpk_e4e708a080ab11e99cf7e73f32bcec42",secretkey:"tsk_e4e708a180ab11e99cf7e73f32bcec42",sandbox:true})
+let k=kkiapay({publickey:"xxxxxxx",privatekey:"xxxxxxxxx",secretkey:"xxxxxxxxx",sandbox:true})
 
 /*k.setup_payout({algorithm : "rate", send_notification : true, destination_type : "MOBILE_MONEY", rate_frequency : "1m", destination : "22997000000" }).
 then(e=>console.log(e)).
@@ -10,9 +10,9 @@ catch(err=>console.log(err))*/
 then(e=>console.log(e)).
 catch(err=>console.log(err))*/
 
-/*k.setup_payout({algorithm : "roof", send_notification : true, destination_type : "MOBILE_MONEY", roof_amount : "1000", destination : "22997000000" }).
+k.setup_payout({algorithm : "roof", send_notification : true, destination_type : "MOBILE_MONEY", roof_amount : "1000", destination : "22997000000" }).
 then(e=>console.log(e)).
-catch(err=>console.log(err))*/
+catch(err=>console.log(err))
 
 
 
